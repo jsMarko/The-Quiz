8f6b7e53c75ab6dea4c90d73e0debd6c85641c49
Mark Branscum <mbstudios@hotmail.com>
2020-08-07 01:47:18 -0500

    Final Update

--- js/app.js
+++ js/app.js
@@ -8,11 +8,12 @@ const questions = [
 ];
 
 //  store number of questions answered correctly
-
+let correct = [];
+let incorrect = [];
 let correctAnswers = 0;
 
 /* loop through questions 
-     present question to user via prompot
+     present question to user via prompt
      compare user response to answer in the array
      if response matches answer then number of correct answers increments by 1
 */
@@ -24,10 +25,26 @@ for (let i = 0; i < questions.length; i++) {
 
    if (response === answer) {
       correctAnswers++;
+      correct.push(question);
+   } else {
+      incorrect.push(question);
    }
 }
 
+function listItems(arr) {
+   let items = "";
+   for (let i = 0; i < arr.length; i++) {
+      items += `<li>${arr[i]}</li>`;
+   }
+   return items;
+}
+
 let html = `
-  <h2> You got ${correctAnswers} question(s) right!!! </h2>
+  <h1> You got ${correctAnswers} question(s) right!!! </h1>
+  <h2>You got these questions right!!!</h2>
+  <ol>${listItems(correct)}</ol>
+
+  <h2>You got these questions wrong!!!</h2>
+  <ol>${listItems(incorrect)}</ol>
 `;
 document.querySelector("main").innerHTML = html;
